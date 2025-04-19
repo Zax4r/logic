a9caf97d79ef2aefbbfa65b73c3c52aeec9fcfdb
Zax4r <zaharbedarik@gmail.com>
2025-04-19 16:23:27 +0300

    feat

--- LogicSolver/src/CONSTANTS.h
+++ LogicSolver/src/CONSTANTS.h
@@ -1,3 +1,16 @@
+/*
+Лабораторная работа №1
+Выполнил студент 321701 группы
+Бедарик Захар Александрович
+Вариант 4
+
+Основной заголовочный файл
+19.04.2025
+
+Источники:
+-Проектирование программных основ интеллекутальных систем
+*/
+
 #pragma once
 #ifndef CONSTANTS_H  // Проверяем, был ли уже включен этот файл
 #define CONSTANTS_H  // Обозначаем, что файл включён
@@ -12,14 +25,21 @@
 #include <cmath>
 #include <set>
 #include <chrono>
+#include <unordered_set>
 
 using namespace std;
 
-const string UNARY_OPERANDS = "!";
-const string BINARY_OPERANDS = "&|>~";
+const char UNARY_OPERANDS = '!';
+const string BINARY_OPERANDS = "&|<~";
 const string LITERALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const unordered_set<char> binary_ops = { '&', '|', '<', '~' };
+const unordered_set<char> logic_consts = { '0', '1' };
+const unordered_set<char> literals_set = {
+    'A','B','C','D','E','F','G','H','I','J','K','L','M',
+    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+};
 const string LOGIC_CONSTS = "01";
-const string ALL_OPERANDS = "!&|>~";
+const string ALL_OPERANDS = "!&|<~";
 const string ARGUMENTS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01";
 const char OPEN_BRACKET = '(';
 const char CLOSE_BRACKET = ')';
@@ -27,7 +47,7 @@ const char CLOSE_BRACKET = ')';
 const map<char, int> PRIORITY = {
     {'&', 5},
     {'|', 4},
-    {'>', 3},
+    {'<', 3},
     {'~', 3},
     {'(', 1}
 };
